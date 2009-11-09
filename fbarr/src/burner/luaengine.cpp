e9f02e4fcec54d5eb080fcdbac1e62afa40b44d9
mauzus <mauzus@gmail.com>
2009-11-09 14:30:51 +0000

    Apply recent lua changes of snes9x: * accept a table for color value. * add gui.parsecolor, joypad.getup, joypad.getdown. * gui.line, gui.box, joypad.get become like Gens. * win32: add a menu to Lua window, add "Font..." and "Clear" items.

--- fbarr/src/burner/luaengine.cpp
+++ fbarr/src/burner/luaengine.cpp
@@ -788,8 +788,7 @@ static int memory_registerwrite(lua_State *L) {
 // table joypad.read()
 //
 //  Reads the joypads as inputted by the user.
-//  This is really the only way to get input to the system.
-static int joypad_read(lua_State *L) {
+static int joy_get_internal(lua_State *L, bool reportUp, bool reportDown) {
 	unsigned int i = 0;
 	struct GameInp* pgi = NULL;
 	unsigned short nThisVal;
@@ -803,11 +802,6 @@ static int joypad_read(lua_State *L) {
 		}
 		struct BurnInputInfo bii;
 
-		if (bRunPause) {
-			nThisVal = pgi->Input.nVal;
-		} else {
-			nThisVal = *pgi->Input.pVal;
-		}
 		// Get the name of the input
 		bii.szName = NULL;
 		BurnDrvGetInputInfo(&bii, i);
@@ -820,14 +814,36 @@ static int joypad_read(lua_State *L) {
 			bii.szName = "";
 		}
 
-		if (nThisVal != 0) {
+		nThisVal = *pgi->Input.pVal;
+		bool pressed = nThisVal != 0;
+		if ((pressed && reportDown) || (!pressed && reportUp)) {
 			lua_pushinteger(L,nThisVal);
 			lua_setfield(L, -2, bii.szName);
 		}
 	}
 	return 1;
 }
-
+// joypad.get(which)
+// returns a table of every game button,
+// true meaning currently-held and false meaning not-currently-held
+// (as of last frame boundary)
+// this WILL read input from a currently-playing movie
+static int joypad_get(lua_State *L)
+{
+	return joy_get_internal(L, true, true);
+}
+// joypad.getdown(which)
+// returns a table of every game button that is currently held
+static int joypad_getdown(lua_State *L)
+{
+	return joy_get_internal(L, false, true);
+}
+// joypad.getup(which)
+// returns a table of every game button that is not currently held
+static int joypad_getup(lua_State *L)
+{
+	return joy_get_internal(L, true, false);
+}
 
 // joypad.set(table buttons)
 //
@@ -1086,6 +1102,10 @@ static void gui_prepare() {
 #define DECOMPOSE_PIXEL_ARGB8888(PIX,A,R,G,B) { (A) = ((PIX) >> 24) & 0xff; (R) = ((PIX) >> 16) & 0xff; (G) = ((PIX) >> 8) & 0xff; (B) = (PIX) & 0xff; }
 #define LUA_BUILD_PIXEL BUILD_PIXEL_ARGB8888
 #define LUA_DECOMPOSE_PIXEL DECOMPOSE_PIXEL_ARGB8888
+#define LUA_PIXEL_A(PIX) (((PIX) >> 24) & 0xff)
+#define LUA_PIXEL_R(PIX) (((PIX) >> 16) & 0xff)
+#define LUA_PIXEL_G(PIX) (((PIX) >> 8) & 0xff)
+#define LUA_PIXEL_B(PIX) ((PIX) & 0xff)
 
 // I'm going to use this a lot in here
 #define swap(T, one, two) { \
@@ -1244,6 +1264,7 @@ static void gui_drawbox_internal(int x1, int y1, int x2, int y2, UINT32 colour) 
 	gui_drawline_internal(x2, y1, x2, y2, TRUE, colour);
 }
 
+/*
 // draw a circle on gui_data
 static void gui_drawcircle_internal(int x0, int y0, int radius, UINT32 colour) {
 
@@ -1315,6 +1336,7 @@ static void gui_drawcircle_internal(int x0, int y0, int radius, UINT32 colour) {
 			break;
 	}
 }
+*/
 
 // draw fill rect on gui_data
 static void gui_fillbox_internal(int x1, int y1, int x2, int y2, UINT32 colour) {
@@ -1343,6 +1365,7 @@ static void gui_fillbox_internal(int x1, int y1, int x2, int y2, UINT32 colour) 
 	}
 }
 
+/*
 // fill a circle on gui_data
 static void gui_fillcircle_internal(int x0, int y0, int radius, UINT32 colour) {
 
@@ -1405,7 +1428,7 @@ static void gui_fillcircle_internal(int x0, int y0, int radius, UINT32 colour) {
 			break;
 	}
 }
-
+*/
 
 static const struct ColorMapping
 {
@@ -1486,6 +1509,34 @@ static inline UINT32 gui_getcolour_wrapped(lua_State *L, int offset, UINT8 hasDe
 			UINT32 colour = (UINT32) lua_tointeger(L,offset);
 			return colour;
 		}
+	case LUA_TTABLE:
+		{
+			int color = 0xFF;
+			lua_pushnil(L); // first key
+			int keyIndex = lua_gettop(L);
+			int valueIndex = keyIndex + 1;
+			while(lua_next(L, offset))
+			{
+				bool keyIsString = (lua_type(L, keyIndex) == LUA_TSTRING);
+				bool keyIsNumber = (lua_type(L, keyIndex) == LUA_TNUMBER);
+				int key = keyIsString ? tolower(*lua_tostring(L, keyIndex)) : (keyIsNumber ? lua_tointeger(L, keyIndex) : 0);
+				int value = lua_tointeger(L, valueIndex);
+				if(value < 0) value = 0;
+				if(value > 255) value = 255;
+				switch(key)
+				{
+				case 1: case 'r': color |= value << 24; break;
+				case 2: case 'g': color |= value << 16; break;
+				case 3: case 'b': color |= value << 8; break;
+				case 4: case 'a': color = (color & ~0xFF) | value; break;
+				}
+				lua_pop(L, 1);
+			}
+			return color;
+		}	break;
+	case LUA_TFUNCTION:
+		luaL_error(L, "invalid colour"); // NYI
+		return 0;
 	default:
 		if (hasDefaultValue)
 			return defaultColour;
@@ -1540,55 +1591,54 @@ static int gui_drawpixel(lua_State *L) {
 	return 0;
 }
 
-// gui.drawline(x1,y1,x2,y2,type colour)
+// gui.drawline(x1,y1,x2,y2,color,skipFirst)
 static int gui_drawline(lua_State *L) {
 
 	int x1,y1,x2,y2;
-	UINT32 colour;
+	UINT32 color;
 	x1 = luaL_checkinteger(L,1);
 	y1 = luaL_checkinteger(L,2);
 	x2 = luaL_checkinteger(L,3);
 	y2 = luaL_checkinteger(L,4);
-	colour = gui_getcolour(L,5);
-
-//	if (!gui_check_boundary(x1, y1))
-//		luaL_error(L,"bad coordinates");
-//
-//	if (!gui_check_boundary(x2, y2))
-//		luaL_error(L,"bad coordinates");
+	color = gui_optcolour(L,5,LUA_BUILD_PIXEL(255, 255, 255, 255));
+	int skipFirst = lua_toboolean(L,6);
 
 	gui_prepare();
 
-	gui_drawline_internal(x1, y1, x2, y2, TRUE, colour);
+	gui_drawline_internal(x2, y2, x1, y1, !skipFirst, color);
 
 	return 0;
 }
 
-// gui.drawbox(x1, y1, x2, y2, colour)
+// gui.drawbox(x1, y1, x2, y2, fillcolor, outlinecolor)
 static int gui_drawbox(lua_State *L) {
 
 	int x1,y1,x2,y2;
-	UINT32 colour;
+	UINT32 fillcolor;
+	UINT32 outlinecolor;
 
 	x1 = luaL_checkinteger(L,1);
 	y1 = luaL_checkinteger(L,2);
 	x2 = luaL_checkinteger(L,3);
 	y2 = luaL_checkinteger(L,4);
-	colour = gui_getcolour(L,5);
+	fillcolor = gui_optcolour(L,5,LUA_BUILD_PIXEL(63, 255, 255, 255));
+	outlinecolor = gui_optcolour(L,6,LUA_BUILD_PIXEL(255, LUA_PIXEL_R(fillcolor), LUA_PIXEL_G(fillcolor), LUA_PIXEL_B(fillcolor)));
 
-//	if (!gui_check_boundary(x1, y1))
-//		luaL_error(L,"bad coordinates");
-//
-//	if (!gui_check_boundary(x2, y2))
-//		luaL_error(L,"bad coordinates");
+	if (x1 > x2)
+		std::swap<int>(x1, x2);
+	if (y1 > y2)
+		std::swap<int>(y1, y2);
 
 	gui_prepare();
 
-	gui_drawbox_internal(x1, y1, x2, y2, colour);
+	gui_drawbox_internal(x1, y1, x2, y2, outlinecolor);
+	if ((x2 - x1) >= 2 && (y2 - y1) >= 2)
+		gui_fillbox_internal(x1+1, y1+1, x2-1, y2-1, fillcolor);
 
 	return 0;
 }
 
+/*
 // gui.drawcircle(x0, y0, radius, colour)
 static int gui_drawcircle(lua_State *L) {
 
@@ -1649,7 +1699,7 @@ static int gui_fillcircle(lua_State *L) {
 
 	return 0;
 }
-
+*/
 
 static int gui_getpixel(lua_State *L) {
 	int x = luaL_checkinteger(L, 1);
@@ -1701,6 +1751,17 @@ static int gui_getpixel(lua_State *L) {
 	return 3;
 }
 
+static int gui_parsecolor(lua_State *L)
+{
+	int r, g, b, a;
+	UINT32 color = gui_getcolour(L,1);
+	LUA_DECOMPOSE_PIXEL(color, a, r, g, b);
+	lua_pushinteger(L, r);
+	lua_pushinteger(L, g);
+	lua_pushinteger(L, b);
+	lua_pushinteger(L, a);
+	return 4;
+}
 
 
 // gui.gdscreenshot()
@@ -3000,10 +3061,14 @@ static const struct luaL_reg memorylib [] = {
 };
 
 static const struct luaL_reg joypadlib[] = {
-	{"get", joypad_read},
+	{"get", joypad_get},
+	{"getdown", joypad_getdown},
+	{"getup", joypad_getup},
 	{"set", joypad_set},
 	// alternative names
-	{"read", joypad_read},
+	{"read", joypad_get},
+	{"readdown", joypad_getdown},
+	{"readup", joypad_getup},
 	{"write", joypad_set},
 	{NULL,NULL}
 };
@@ -3034,12 +3099,10 @@ static const struct luaL_reg guilib[] = {
 	{"box", gui_drawbox},
 	{"line", gui_drawline},
 	{"pixel", gui_drawpixel},
-	{"circle", gui_drawcircle},
 	{"opacity", gui_setopacity},
-	{"fillbox", gui_fillbox},
-	{"fillcircle", gui_fillcircle},
 	{"transparency", gui_transparency},
 	{"popup", gui_popup},
+	{"parsecolor", gui_parsecolor},
 	{"gdscreenshot", gui_gdscreenshot},
 	{"gdoverlay", gui_gdoverlay},
 	{"getpixel", gui_getpixel},
@@ -3051,7 +3114,6 @@ static const struct luaL_reg guilib[] = {
 	{"drawpixel", gui_drawpixel},
 	{"setpixel", gui_drawpixel},
 	{"writepixel", gui_drawpixel},
-	{"drawcircle", gui_drawcircle},
 	{"rect", gui_drawbox},
 	{"drawrect", gui_drawbox},
 	{"drawimage", gui_gdoverlay},
