7bcde211e8fb4f0eb2df744dbe8424ef02db8266
andres.delikat <andres.delikat@4f69235d-9255-0410-a8be-7de0231a674a>
2010-08-18 03:24:13 +0000

    Bind movies to savestates, TODO: a way to toggle this feature

--- trunk/fbarr/src/burner/win32/replay.h
+++ trunk/fbarr/src/burner/win32/replay.h
@@ -3,3 +3,4 @@
 void UpdateFrameCounter();			//Updates the frame counter display on screen
 int GetTotalMovieFrames();			//Returns total length of the movie playing, or 0 if no movie
 std::wstring GetCurrentMovie();		//Returns the name of the current movie loaded
+bool MovieIsActive();				//If a movie is loaded or not
