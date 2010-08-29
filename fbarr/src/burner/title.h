3217cd726716c23e76ecd68976c7b3523201daa8
mauzus <mauzus@gmail.com>
2010-08-29 13:06:58 +0000

    clean up

--- fbarr/src/burner/title.h
+++
@@ -1,9 +0,0 @@
-// Define macros for appliction title and description
-#ifdef FBA_DEBUG
- #define APP_TITLE "FBA-RR [DEBUG]"
-#else
- #define APP_TITLE "FBA-RR"
-#endif
-
-#define APP_DESCRIPTION "Emulator for MC68000/Z80 based arcade games"
-
