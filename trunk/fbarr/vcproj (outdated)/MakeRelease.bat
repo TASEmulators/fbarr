8c56b36a70d84a3ccc40c474f48b193aadc523a6
mauzus <mauzus@4f69235d-9255-0410-a8be-7de0231a674a>
2011-10-12 15:55:31 +0000

    delete everything

--- trunk/fbarr/vcproj (outdated)/MakeRelease.bat
+++
@@ -1,3 +0,0 @@
-zip -X -9 -r fba-rr.zip output\LuaScripts\*.lua output\ output\docs\ output\roms\
-
-
--- trunk/fbarr/vcproj (outdated)/src/burn/namco_snd.h
+++
@@ -1,6 +0,0 @@
-extern unsigned char* NamcoSoundProm;
-void NamcoSoundUpdate(short* buffer, int length);
-void NamcoSoundWrite(unsigned int offset, unsigned char data);
-void NamcoSoundInit(int clock);
-void NamcoSoundExit();
-void NamcoSoundScan(int nAction,int *pnMin);
