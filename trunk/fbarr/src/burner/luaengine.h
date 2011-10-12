3cdd7628488bd53baf96d73c917b0245814473d3
mauzus <mauzus@4f69235d-9255-0410-a8be-7de0231a674a>
2011-10-12 16:01:52 +0000

    - add everything - update to FB Alpha 0.2.97.11 It still needs more work, since most games don't even load and it crashes a lot.

---
+++ trunk/fbarr/src/burner/luaengine.h
@@ -0,0 +1,47 @@
+#ifndef _LUAENGINE_H
+#define _LUAENGINE_H
+
+enum LuaCallID
+{
+	LUACALL_BEFOREEMULATION,
+	LUACALL_AFTEREMULATION,
+	LUACALL_BEFOREEXIT,
+	LUACALL_ONSTART,
+
+	LUACALL_HOTKEY_1,
+	LUACALL_HOTKEY_2,
+	LUACALL_HOTKEY_3,
+	LUACALL_HOTKEY_4,
+	LUACALL_HOTKEY_5,
+
+	LUACALL_COUNT
+};
+void CallRegisteredLuaFunctions(int calltype);
+
+//void FBA_LuaWrite(UINT32 addr);
+void FBA_LuaFrameBoundary();
+int FBA_LoadLuaCode(const char *filename);
+void FBA_ReloadLuaCode();
+void FBA_LuaStop();
+int FBA_LuaRunning();
+
+int FBA_LuaUsingJoypad();
+UINT32 FBA_LuaReadJoypad();
+int FBA_LuaSpeed();
+//int FBA_LuaFrameskip();
+int FBA_LuaRerecordCountSkip();
+
+void FBA_LuaGui(unsigned char *s, int width, int height, int bpp, int pitch);
+
+void FBA_LuaWriteInform();
+
+void FBA_LuaClearGui();
+void FBA_LuaEnableGui(UINT8 enabled);
+
+char* FBA_GetLuaScriptName();
+struct lua_State* FBA_GetLuaState();
+
+void luasav_save(const char *filename);
+void luasav_load(const char *filename);
+
+#endif
